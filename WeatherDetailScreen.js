45e1aae07ff92b50e5313289d37af80245619249
leehyunjun <zxzxs9182@gmail.com>
2019-06-14 00:00:45 +0900

    add humidity & comment

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -5,7 +5,7 @@ import { Constants } from 'expo';
 export default class WeatherDetailScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
-      title: `Weather Info: ${navigation.getParam('city', 'Unknown')}`,
+      title: `Weather Info of ${navigation.getParam('city', 'Unknown')}`,
     };
   };
 
@@ -45,14 +45,24 @@ export default class WeatherDetailScreen extends React.Component {
     let celsius = this.state.main.temp - 273.15;
     let icon = this.state.weather[0].icon;
     let windspeed = this.state.wind.speed;
+    let humidity = this.state.main.humidity;
     var comment;
    
-    if(celsius<20){
-      comment= "날이 춥네요";
+    if(celsius<0){
+      comment = "엄청 추운 날씨입니다.\n 꽁꽁 싸메고 가세요~!\n";
     }
-    else{
-      comment="날이 덥네요";
+    else if (celsius<10){
+      comment = "적당히 추운 날씨네요.\n 두꺼운 외투 하나 챙기시길~ \n";
     }
+    else if(celsius<20){
+      comment = "선선한 날씨네요.\n 가디건 챙기시는거 추천드립니다~\n";
+    }
+    else if(celsius<30){
+      comment = "날씨가 점점 더워지네요.......";
+      }
+    else {
+              comment = "너무 더운 날씨...... 집이 최고...";
+        }
     return (
       <View style={styles.container}>
         <Text style={styles.weather}>{celsius.toFixed(1)}°</Text>
@@ -60,7 +70,10 @@ export default class WeatherDetailScreen extends React.Component {
             style ={styles.img}
             source = {{uri: `http://openweathermap.org/img/w/${icon}.png`}}
             />
+      
         <Text style={styles.ws}>바람 세기 : {windspeed}m/s</Text>
+        <Text style={styles.ws}>습도 : {humidity}%</Text>
+        <Text style={styles.cmt}>{comment}</Text>
       </View>
     );
   }
@@ -69,25 +82,33 @@ export default class WeatherDetailScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     marginTop: Constants.statusBarHeight,
-   
+    
     
   },
   ws:{
+    color: 'white',
     textAlign: 'right',
     fontSize: 20,
     marginRight : 10,
   },
   weather:{
-    fontSize : 120,
+    color: 'white',
+    fontSize : 100,
+    textAlign : "center",
+    fontWeight: 'bold',
+  },
+  cmt:{
+    color: 'white',
+    fontSize : 20,
     textAlign : "center",
     fontWeight: 'bold',
   },
   img:{
     alignSelf: 'flex-start',
       width : 200,
-      height: 200,
+      height: 180,
      marginLeft : 110,
 
   }
